10a5a18ad8ea39c139f841c363e63c0c4b99f6c3
ContursiF <fabio.contursi@gmail.com>
2018-06-19 22:17:08 +0200

    v2

---
+++ app/src/main/java/dev/contursif/app4study/Activity_Fragment_hw.java
@@ -0,0 +1,22 @@
+package dev.contursif.app4study;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_Fragment_hw extends AppCompatActivity {
+/*
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_fragment_hw);
+            if (savedInstanceState == null)
+            {
+               // getSupportFragmentManager().beginTransaction()
+                      //  .add(R.id.container, new Fragment_hw()).commit();
+
+            }
+        }
+    }
+*/
+}
---
+++ app/src/main/java/dev/contursif/app4study/Fragment_hw.java
@@ -0,0 +1,23 @@
+package dev.contursif.app4study;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_hw  extends Fragment {
+
+    public Fragment_hw() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_hw, container, false);
+        return rootView;
+
+    }
+
+}
